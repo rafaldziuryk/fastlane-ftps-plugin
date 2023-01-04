77728fbb3ec394a65b70d8830ba4306822a8ef62
rafaldziuryk <rafal.dziuryk@appvinio.com>
2023-01-04 21:05:28 +0100

    fix ftps names

--- fastlane-plugin-ftps.gemspec
+++ fastlane-plugin-ftps.gemspec
@@ -1,11 +1,11 @@
 # coding: utf-8
 lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'fastlane/plugin/ftp/version'
+require 'fastlane/plugin/ftps/version'
 
 Gem::Specification.new do |spec|
   spec.name          = 'fastlane-plugin-ftps'
-  spec.version       = Fastlane::Ftp::VERSION
+  spec.version       = Fastlane::Ftps::VERSION
   spec.author        = %q{Rafał Dziuryk}
   spec.email         = %q{rafal.dziuryk@appvinio.com}
 
--- lib/fastlane/plugin/ftps.rb
+++ lib/fastlane/plugin/ftps.rb
@@ -1,7 +1,7 @@
-require 'fastlane/plugin/ftp/version'
+require 'fastlane/plugin/ftps/version'
 
 module Fastlane
-  module Ftp
+  module Ftps
     # Return all .rb files inside the "actions" and "helper" directory
     def self.all_classes
       Dir[File.expand_path('**/{actions,helper}/*.rb', File.dirname(__FILE__))]
@@ -11,6 +11,6 @@ end
 
 # By default we want to import all available actions and helpers
 # A plugin can contain any number of actions and plugins
-Fastlane::Ftp.all_classes.each do |current|
+Fastlane::Ftps.all_classes.each do |current|
   require current
 end
